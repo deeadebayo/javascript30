3199eb9dfede31c55c61b560f65696a3a9d3e0a9
deeadebayo <deeadebayo@gmail.com>
2019-02-12 01:46:32 -0500

    finished 1

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,29 @@
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+// function playClickSound(e) {
+//     const clicked = e.target;
+//     console.log(clicked);
+    
+//     // if(clicked[])
+
+//     // const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//     // const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+// }
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+// keys.forEach(key => key.addEventListener('click', playClickSound));
+window.addEventListener('keydown', playSound);
